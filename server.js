e572cc2c10bbb3c77a3ca21ede5776f1f7268abb
Anton Nikolenko <nikolenkoanton92@gmail.com>
2016-08-11 23:27:43 -0700

    add template index view, register hapi plugins (inert,vision), listen to pre response

--- server.js
+++ server.js
@@ -1,17 +1,41 @@
-import Hapi from 'Hapi';
+import Hapi from 'hapi';
+import Inert from 'inert';
+import Vision from 'vision';
+import path from 'path';
 const server = new Hapi.Server();
+
 
 server.connection({
   host: 'localhost',
   port: 8080
 });
 
+server.register([Inert, Vision], (err) => {
+  if (err)
+    throw err;
+});
+
+server.views({
+  engines: {
+    html: require('handlebars')
+  },
+  path: path.join(__dirname, 'views')
+});
+
+/**
+ * serve static requests from the public folder
+ */
+
 server.route({
   method: 'GET',
-  path: '/',
-  handler: function(request, reply) {
-    reply('Hello World');
+  path: '/{params*}',
+  handler: {
+    file: (request) => 'public' + request.path
   }
+});
+
+server.ext('onPreResponse', (request, reply) => {
+  reply.view('index');
 });
 
 server.start(() => {
